35ec6371e93d6c0449d63c5ddca6d35b0907c588
sohaibrahman64 <sohaib.rahman64@gmail.com>
2022-09-26 21:16:15 +0530

    India related categories

--- app/constants.py
+++ app/constants.py
@@ -2,12 +2,12 @@ NEWS_CATEGORIES = [
     {'id': 1, 'name': 'India Top Stories'},
     {'id': 2, 'name': 'India'},
     {'id': 3, 'name': 'World'},
-    {'id': 4, 'name': 'Business'},
-    {'id': 5, 'name': 'Tech'},
+    {'id': 4, 'name': 'India Business'},
+    {'id': 5, 'name': 'India Tech'},
     {'id': 6, 'name': 'Bollywood'},
-    {'id': 7, 'name': 'Sports'},
-    {'id': 8, 'name': 'Science'},
-    {'id': 9, 'name': 'Health'},
+    {'id': 7, 'name': 'India Sports'},
+    {'id': 8, 'name': 'India Science'},
+    {'id': 9, 'name': 'India Health'},
 ]
 
 URL_PREFIX = '/api/v1'
